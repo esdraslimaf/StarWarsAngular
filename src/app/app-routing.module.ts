3e7a982eea5bb7b660dd022221a43fae9f2e1bed
esdraslimaf <esdraslimaf@gmail.com>
2024-05-08 00:32:31 -0300

    Reorganizando pastas + melhorando css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CharactersComponent } from './components/characters/characters.component';
-import { CharacterinfosComponent } from './components/characterinfos/characterinfos.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { CharacterinfosComponent } from './pages/characterinfos/characterinfos.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CharactersComponent } from './components/characters/characters.component';
+import { CharactersComponent } from './pages/characters/characters.component';
 import { HttpClientModule } from '@angular/common/http';
 import { InfosCharacterListComponent } from './components/infos-character-list/infos-character-list.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { CharacterinfosComponent } from './components/characterinfos/characterinfos.component';
+import { CharacterinfosComponent } from './pages/characterinfos/characterinfos.component';
 
 @NgModule({
   declarations: [
