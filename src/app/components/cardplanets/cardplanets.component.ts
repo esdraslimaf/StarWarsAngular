da8881c9e2486ac70b38aa767aa3ab00f9fa507a
esdraslimaf <esdraslimaf@gmail.com>
2024-05-08 03:11:25 -0300

    Puxando id do planeta e mostrando no planetcard

--- src/app/components/cardplanets/cardplanets.component.ts
+++ src/app/components/cardplanets/cardplanets.component.ts
@@ -1,12 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cardplanets',
   templateUrl: './cardplanets.component.html',
   styleUrl: './cardplanets.component.scss'
 })
-export class CardplanetsComponent {
-@Input() planet:any;
+export class CardplanetsComponent implements OnInit{
 
+@Input() planet:any;
+planetId: string = '';
+
+ngOnInit(): void {
+  this.planetId = this.getIdFromUrl(this.planet.url);
+  console.log(this.planetId)
+}
+
+getIdFromUrl(url: string): string {
+  const parts = url.split('/');
+  return parts[parts.length - 2]; 
+}
 
 }
